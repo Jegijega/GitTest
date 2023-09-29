1594a2f9e6810dfdfecc7fc3c1659469f9f0833c
S <s>
2023-09-29 14:38:31 +0400

    {"types":"C","runs":7}

--- mine01.java
+++ mine01.java
@@ -2,14 +2,15 @@ import stanford.karel.Karel;
 
 public class mine01 extends Karel{
 	public void run(){
-		for(int i = 0; i < 4; i++){
-			for(int j = 0; j < 9; j++){
-				putBeeper();
-				if( i < 3 || j < 8){
-				move();
+		//for(int p = 0; p < )
+			for(int i = 0; i < 4; i++){
+				for(int j = 0; j < 9; j++){
+					putBeeper();
+					if( i < 3 || j < 8){
+					move();
+					}
 				}
+			turnLeft();
 			}
-		turnLeft();
-		}
 	}
 }
