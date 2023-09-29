77b9e724d08ce7459aed8f8212ab359094de3384
S <s>
2023-09-29 14:43:35 +0400

    {"types":"C","runs":10}

--- mine01.java
+++ mine01.java
@@ -4,9 +4,9 @@ public class mine01 extends Karel{
 	public void run(){
 		for(int p = 0; p < 5; p++){
 			for(int i = 0; i < 4; i++){
-				for(int j = 0; j < 9-p; j++){
+				for(int j = 0; j < 9-2*p; j++){
 					putBeeper();
-					if( i < 3 || j < 8-p){
+					if( i < 3 || j < 8-2*p){
 					move();
 					}
 				}
