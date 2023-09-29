3828118df0cb0fc584a553cfca6e3a2125cec94a
S <s>
2023-09-29 15:12:30 +0400

    {"types":"C","runs":21}

--- mine02.java
+++ mine02.java
@@ -32,5 +32,25 @@ public class mine02 extends Karel{
 		for(int i = 0; i < 4; i++){
 			move();
 		}
+		for(int j = 0; j < 5; j++){
+			putBeeper();
+			move();
+			putBeeper();
+			turnLeft();
+			move();
+			putBeeper();
+			turnLeft();
+			move();
+			putBeeper();
+			if(j < 4){
+			turnLeft();
+			turnLeft();
+			turnLeft();
+			move();
+			turnLeft();
+			turnLeft();
+			turnLeft();
+			}
+		}
 	}
 }
