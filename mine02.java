f6c81fb9c347b35d6600e7c7c452597be5c5462d
S <s>
2023-09-29 15:05:08 +0400

    {"types":"C","runs":12}

--- mine02.java
+++ mine02.java
@@ -1,4 +1,22 @@
+import stanford.karel.Karel;
 
-public class mine02 {
-
+public class mine02 extends Karel{
+	public void run(){
+		for(int i = 0; i < 4; i++){
+			move();
+		}
+		for(int j = 0; j < 5; j++){
+			putBeeper();
+			move();
+			putBeeper();
+			turnLeft();
+			move();
+			putBeeper();
+			turnLeft();
+			move();
+			putBeeper();
+			turnLeft();
+			turnLeft();
+		}
+	}
 }
