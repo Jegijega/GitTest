4d823a49bd2362b143b87e88b33d309a1e91fa47
S <s>
2023-10-02 13:36:50 +0400

    {"types":"C","runs":2}

--- mine03.java
+++ mine03.java
@@ -1,4 +1,30 @@
+import stanford.karel.SuperKarel;
 
-public class mine03 {
-
+public class mine03 extends SuperKarel {
+	public void run(){	
+		int h=0, w=0;
+		
+		while(h < 10 || w < 10){
+			for(int i = 0; i < 3; i++){
+				if(rightIsClear()){
+					turnRight();
+					move();
+					break;
+				}
+				if(frontIsClear()){
+					move();
+					break;
+				}
+				if(leftIsClear()){
+					turnLeft();
+					move();
+					break;
+				} else{
+					turnLeft();
+					turnLeft();
+					move();
+					}
+			}
+		}	
+	}
 }
