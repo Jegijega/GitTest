de9bc6298c0907b8f0e92a19edbd33ebcd41ab68
S <s>
2023-10-02 13:43:29 +0400

    {"types":"C","runs":4}

--- mine03.java
+++ mine03.java
@@ -2,27 +2,75 @@ import stanford.karel.SuperKarel;
 
 public class mine03 extends SuperKarel {
 	public void run(){	
-		int h=0, w=0;
+		int h=1, w=1;
 		
 		while(h < 10 || w < 10){
 			for(int i = 0; i < 3; i++){
 				if(rightIsClear()){
 					turnRight();
 					move();
+					if(facingNorth()){
+						h++;
+					}
+					if(facingSouth()){
+						h--;
+					}
+					if(facingEast()){
+						w++;
+					}
+					if(facingWest()){
+						w--;
+					}
 					break;
 				}
 				if(frontIsClear()){
 					move();
+					if(facingNorth()){
+						h++;
+					}
+					if(facingSouth()){
+						h--;
+					}
+					if(facingEast()){
+						w++;
+					}
+					if(facingWest()){
+						w--;
+					}
 					break;
 				}
 				if(leftIsClear()){
 					turnLeft();
 					move();
+					if(facingNorth()){
+						h++;
+					}
+					if(facingSouth()){
+						h--;
+					}
+					if(facingEast()){
+						w++;
+					}
+					if(facingWest()){
+						w--;
+					}
 					break;
 				} else{
 					turnLeft();
 					turnLeft();
 					move();
+					if(facingNorth()){
+						h++;
+					}
+					if(facingSouth()){
+						h--;
+					}
+					if(facingEast()){
+						w++;
+					}
+					if(facingWest()){
+						w--;
+					}
 					}
 			}
 		}	
