71b10c62c0ecb53f52800fb6392bdf7f149d58a0
S <s>
2023-10-03 13:57:23 +0400

    {"types":"C","runs":31}

--- Problem11.java
+++ Problem11.java
@@ -1,4 +1,79 @@
+import stanford.karel.SuperKarel;
 
-public class Problem11 {
-
+public class Problem11 extends SuperKarel {
+	public void run(){	
+		int n=1, e=1;
+		
+		while(n < 10 || e < 10){
+			for(int i = 0; i < 3; i++){
+				if(rightIsClear()){
+					turnRight();
+					move();
+					if(facingNorth()){
+						n++;
+					}
+					if(facingSouth()){
+						n--;
+					}
+					if(facingEast()){
+						e++;
+					}
+					if(facingWest()){
+						e--;
+					}
+					break;
+				}
+				if(frontIsClear()){
+					move();
+					if(facingNorth()){
+						n++;
+					}
+					if(facingSouth()){
+						n--;
+					}
+					if(facingEast()){
+						e++;
+					}
+					if(facingWest()){
+						e--;
+					}
+					break;
+				}
+				if(leftIsClear()){
+					turnLeft();
+					move();
+					if(facingNorth()){
+						n++;
+					}
+					if(facingSouth()){
+						n--;
+					}
+					if(facingEast()){
+						e++;
+					}
+					if(facingWest()){
+						e--;
+					}
+					break;
+				} else{
+					turnLeft();
+					turnLeft();
+					move();
+					if(facingNorth()){
+						n++;
+					}
+					if(facingSouth()){
+						n--;
+					}
+					if(facingEast()){
+						e++;
+					}
+					if(facingWest()){
+						e--;
+					}
+					}
+			}
+		}	
+		turnRight();
+	}
 }
