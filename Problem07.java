1293ea626386ffead40ed9d73d37723642abc5ce
S <s>
2023-10-03 18:36:07 +0400

    {"types":"C","runs":33}

--- Problem07.java
+++ Problem07.java
@@ -4,34 +4,43 @@ public class Problem07 extends Karel{
 	public void run(){
 		while(frontIsClear()){
 			while(frontIsClear()){
-				if(noBeepersPresent()){
-					putBeeper();
-				}
+				checkBeepersAndPut();
 				move();
 			}
 			if(facingEast()){
-				if(noBeepersPresent()){
-					putBeeper();
-				}
-				turnLeft();
-				if(frontIsClear()){
-				move();
-				turnLeft();
-				}
+				turnAroundFromEast();
 			} else{
-				if(noBeepersPresent()){
-					putBeeper();
-				}
-				turnLeft();
-				turnLeft();
-				turnLeft();
-				if(frontIsClear()){
-				move();
-				turnLeft();
-				turnLeft();
-				turnLeft();
-				}
+				turnAroundFromWest();
 			}
 		}
 	}
+	
+	private void checkBeepersAndPut(){
+		if(noBeepersPresent()){
+			putBeeper();
+		}
+	}
+	
+	private void turnAroundFromEast(){
+		checkBeepersAndPut();
+		turnLeft();
+		if(frontIsClear()){
+			move();
+			turnLeft();
+		}
+	}
+	
+	private void turnAroundFromWest(){
+		checkBeepersAndPut();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		if(frontIsClear()){
+			move();
+			turnLeft();
+			turnLeft();
+			turnLeft();
+		}
+	}
+	
 }
