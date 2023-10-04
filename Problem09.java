ea93f34f797ff68859e67a222f0db9cf1cdc2dcf
S <s>
2023-10-04 12:39:20 +0400

    {"types":"C","runs":60}

--- Problem09.java
+++ Problem09.java
@@ -9,6 +9,16 @@ public class Problem09 extends Karel{
 				plusM();
 			}
 			takingBackM();
+			turnLeft();
+			move();
+			turnLeft();
+			move();
+			move();
+			putBeeper();
+			turnLeft();
+			move();
+			pickBeeper();
+			turnLeft();
 		}
 	}
 	private void plusN(){
