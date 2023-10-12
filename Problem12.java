57f5e872acce1bd87506641a15b53acd874304fe
S <s>
2023-10-12 18:38:17 +0400

    {"types":"C","runs":126}

--- Problem12.java
+++ Problem12.java
@@ -1,17 +1,32 @@
-import acm.graphics.GRect;
+import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
 public class Problem12 extends GraphicsProgram{
+	private static final double HOUSE_WIDTH = 200;
+	private static final double HOUSE_HEIGHT = 200;
+	private static final double WINDOW_WIDTH = 200;
+	private static final double WINDOW_HEIGHT = 200;
+	
 	public void run(){
-		int width = 100;
-		width = width + 20;
-		width = 200;
-		
-		GRect rect = new GRect(width, width, width, width);
-		add(rect);
+		drawMidLine();
 	}
-
-	private void testVoid(){
-		
+	
+	private void drawHouse(){
+	drawWall();
+	drawWindows();
+	drawRoof();
+	drawDoor();
+	}
+	
+	private void drawWall(){
+		GRect wall = new Grect(HOUSE_WIDTH,HOUSE_HEIGHT);
+		double x;
+		double y = getHeight() - HOUSE_HEIGHT;
+	}
+	
+	private void drawMidLine(){
+		double x1 = 0, y1 = getHeight() / 2, x2 = getWidth(), y2 = y1;
+		GLine line = new GLine(x1, y1, x2, y2);
+		add(line);
 	}
 }
