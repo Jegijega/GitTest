ae615583d49a940d486d0b76587a3996e02d20db
S <s>
2023-10-12 19:05:54 +0400

    {"types":"C","runs":156}

--- consoleProgram.java
+++ consoleProgram.java
@@ -4,5 +4,7 @@ public class consoleProgram extends ConsoleProgram {
 	public void run() {
 		int reader = readInt("Enter number");
 		println(reader);
+		String num1 = readLine("Enter number");
+		println(num1);
 	}
 }
