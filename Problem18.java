7943a5b52f4ba91c49121c97b82c43139b7ca851
S <s>
2023-10-17 18:59:15 +0400

    {"types":"C","runs":10}

--- Problem18.java
+++ Problem18.java
@@ -4,10 +4,12 @@ public class Problem18 extends ConsoleProgram{
 	public void run(){
 		int a = readInt("enter first number ");
 		int b = readInt("enter second number ");
-		if(a > b){
-			println(a);
-		}	else{
-			println(b);
-		}
+//		if(a > b){
+//			println(a);
+//		}	else{
+//			println(b);
+//		}
+		int c = Math.max(a,b);
+		println(c);
 	}
 }
