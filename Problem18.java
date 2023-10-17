b2f1541f9cb3a3b93e9af8ecc927755b3fe36f7d
S <s>
2023-10-17 19:01:55 +0400

    {"types":"C","runs":13}

--- Problem18.java
+++ Problem18.java
@@ -11,6 +11,6 @@ public class Problem18 extends ConsoleProgram{
 //		}
 		int c = Math.max(a,b);
 		println(c);
-		int[] arr = readInt("enter array ");
+		int[] arr = {readInt("enter array ")};
 	}
 }
