e2fa983a2de3e7f0ec55082ca45628694c01c8b8
S <s>
2023-10-17 18:52:02 +0400

    {"types":"C","runs":6}

--- Problem17.java
+++ Problem17.java
@@ -1,12 +1,28 @@
+import java.math.BigDecimal;
+
 import acm.program.ConsoleProgram;
 
 public class Problem17 extends ConsoleProgram{
-	public void run(){
+//	public void run(){
+//		double d = readDouble("Enter number ");
+//		int a = (int) d;
+//		double b = d - a;
+//		println(a);
+//		println(b);
+//		
+	
+	}
+	
+	private void calculateInBigDecimal(){
 		double d = readDouble("Enter number ");
-		int a = (int) d;
-		double b = d - a;
-		println(a);
-		println(b);
+		String dAsAString = "" + d;
+		BigDecimal decimal = new BigDecimal(d);
+		println(decimal);
 		
+		int integerPart = decimal.intValue();
+		BigDecimal fractionPart = decimal.remainder(BigDecimal.ONE);
+		
+		println("integerPart: " + integerPart);
+		println("fractionPart: " + fractionPart);
 	}
 }
