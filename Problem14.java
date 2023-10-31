b67087bd877e17177e3555ef1b707b407b706e32
S <s>
2023-10-31 17:09:45 +0400

    {"types":"C","runs":13}

--- Problem14.java
+++ Problem14.java
@@ -1,4 +1,22 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class Problem14 {
-
+public class Problem14 extends GraphicsProgram{
+	private static final double FACE_WIDTH = 200;
+	private static final double FACE_HEIGHT = 200;
+	private static final double EYE_WIDTH = 200;
+	private static final double EYE_HEIGHT = 200;
+	public void run(){
+		drawHead();
+		drawEyes();
+	}
+	
+	private void drawHead(){
+		GRect head = new GRect(FACE_WIDTH,FACE_HEIGHT);
+		add(head, getWidth() / 2 - FACE_WIDTH / 2, getHeight() - FACE_HEIGHT);
+	}
+	
+	private void drawEyes(){
+		
+	}
 }
