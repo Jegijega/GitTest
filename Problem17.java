613e83151b4ce40533639a6963449b29aab95780
S <s>
2023-10-31 17:37:05 +0400

    {"types":"C","runs":28}

--- Problem17.java
+++ Problem17.java
@@ -4,18 +4,16 @@ import acm.program.ConsoleProgram;
 
 public class Problem17 extends ConsoleProgram{
 	public void run(){
-//		double d = readDouble("Enter number ");
-//		int a = (int) d;
-//		double b = d - a;
-//		println(a);
-//		println(b);
-//		
-	
+		calculateInBigDecimal();
+//			double d = readDouble("Enter number ");
+//			int a = (int) d;
+//			double b = d - a;
+//			println(a);
+//			println(b);
 	}
 	
 	private void calculateInBigDecimal(){
 		double d = readDouble("Enter number ");
-		String dAsAString = "" + d;
 		BigDecimal decimal = new BigDecimal(d);
 		println(decimal);
 		
