c19c0fdd0c1d39ac59164a4178f5c4477139cb06
S <s>
2023-11-02 19:46:32 +0400

    {"types":"C","runs":94}

--- Problem22.java
+++ Problem22.java
@@ -1,4 +1,20 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class Problem22 {
+public class Problem22 extends GraphicsProgram{ 	
+	public void run(){
+		double squareX = getWidth() / 8;
+		double squareY = getHeight() / 8;
+		for(int j = 0; j < 8; j++){	
+			for(int i = 0; i < 4; i++){
+				drawSquareB(squareX,squareY, i, j);
+				//drawSquareW();
+			}
+		}	
+	}
 
+	private void drawSquareB(double squareX, double squareY, int X, int Y){
+		GRect rect = new GRect(squareX,squareY);
+		add(rect, squareX * 2 * X, squareY * Y);
+	}
 }
