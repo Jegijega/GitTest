cfcfe2f488e05f60dcaf3e16865087d3ad0d76a5
S <s>
2023-11-02 19:21:48 +0400

    {"types":"C","runs":87}

--- Problem21.java
+++ Problem21.java
@@ -1,4 +1,14 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class Problem21 {
-
+public class Problem21 extends GraphicsProgram{
+	public void run(){
+		for(int i = 0; i < 10; i++ ){
+			
+		}
+	}
+	
+	private void drawingLines(){
+		GLine line = new GLine(X * getWidth() / 9, 0 , X * getWidth / 9, 0 );
+	}
 }
