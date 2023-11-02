a6d205c4241030da77302043598c7881ea8dfcdc
S <s>
2023-11-02 19:54:13 +0400

    {"types":"C","runs":98}

--- Problem22.java
+++ Problem22.java
@@ -8,7 +8,7 @@ public class Problem22 extends GraphicsProgram{
 	public void run(){
 		double squareX = getWidth() / 8;
 		double squareY = getHeight() / 8;
-		for(int j = 0; j < 8; j++){	
+		for(int j = 0; j < 4; j++){	
 			for(int i = 0; i < 4; i++){
 				drawSquareB(squareX,squareY, i, j);
 				//drawSquareW();
@@ -17,10 +17,13 @@ public class Problem22 extends GraphicsProgram{
 	}
 
 	private void drawSquareB(double squareX, double squareY, int X, int Y){
-		GRect rect = new GRect(squareX,squareY);
-		add(rect, squareX * 2 * X, squareY * Y);
-		rect.setFilled(true);
-		rect.setColor(Color.BLACK);
-		
+		GRect rect1 = new GRect(squareX,squareY);
+		add(rect1, squareX * 2 * X, squareY * Y);
+		rect1.setFilled(true);
+		rect1.setColor(Color.BLACK);
+		GRect rect2 = new GRect(squareX,squareY);
+		add(rect2, squareX + squareX * 2 * X, squareY * Y + squareY);
+		rect2.setFilled(true);
+		rect2.setColor(Color.BLACK);
 	}
 }
