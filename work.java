51ffc249f9b07e6dfa07ce204b201fe5dafd84b2
S <s>
2023-11-04 00:07:24 +0400

    {"types":"C","runs":125}

--- work.java
+++ work.java
@@ -1,9 +1,11 @@
+import java.util.Random;
+
 import acm.program.ConsoleProgram;
 import acm.util.RandomGenerator;
 
 public class work extends ConsoleProgram {
 	public void run(){
-		RandomGenerator rand1 = new RandomGenerator();
+		Random rand1 = new Random();
 		println(rand1);
 	}
 }
