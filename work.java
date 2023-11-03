1456492441006b7c15c05df0817d200262fa744c
S <s>
2023-11-04 00:34:32 +0400

    {"types":"C","runs":127}

--- work.java
+++ work.java
@@ -1,11 +1,12 @@
-import java.util.Random;
 
 import acm.program.ConsoleProgram;
 import acm.util.RandomGenerator;
 
+
 public class work extends ConsoleProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	public void run(){
-		println(rgen);
+		double x = rgen.nextDouble(7.5, 8.5);
+		println(x);
 	}
 }
