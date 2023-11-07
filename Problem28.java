fbf43b15535693374255e5dc06fe3a75dd4413e2
S <s>
2023-11-07 16:37:34 +0400

    {"types":"C","runs":31}

--- Problem28.java
+++ Problem28.java
@@ -1,4 +1,21 @@
 
-public class Problem28 {
+import acm.program.ConsoleProgram;
 
+
+public class Problem28 extends ConsoleProgram{
+	public void run(){
+		int n = 0;
+		countEvens(n);
+	}
+
+	private int countEvens (int n) {
+		int count = 0;
+		while(n != -1){
+			n = readInt("? ");
+			if(n % 2 == 0){
+				count++;
+			}
+		}
+		return count;
+	}
 }
