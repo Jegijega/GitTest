45ed27493726086a44a6abf463495a5f34cb1779
S <s>
2023-11-07 15:18:21 +0400

    {"types":"C","runs":25}

--- Problem26.java
+++ Problem26.java
@@ -1,4 +1,22 @@
 
-public class Problem26 {
+import acm.program.ConsoleProgram;
 
+
+public class Problem26 extends ConsoleProgram{
+	public void run(){
+		int n = readInt("Enter first number: ");
+		int m = readInt("Enter second number: ");
+		println(gcd(n,m));
+	}
+
+	private int gcd(int n, int m) {
+		int gcd = n * m - 1;
+		while(gcd > 1){
+			if(n % gcd == 0 && m % gcd == 0){
+				return gcd;
+			}
+			gcd++;
+		}
+		return 1;
+	}
 }
