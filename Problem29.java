ac571159bf5edf166d7a5b06d2ef93697158b20f
S <s>
2023-11-07 17:02:11 +0400

    {"types":"C","runs":33}

--- Problem29.java
+++ Problem29.java
@@ -1,4 +1,26 @@
 
-public class Problem29 {
+import acm.program.ConsoleProgram;
 
+
+public class Problem29 extends ConsoleProgram{
+	public void run(){
+		int n = readInt("Enter a number ");
+		println(counting(n));
+		}
+
+	private int counting (int n) {
+		int count1 = 1;
+		int count2 = 1;
+		int count = 0;
+		if(n < 3){
+			for(int i = 3; i <= n; i++){
+				count = count1 + count2;
+				count1 = count2;
+				count2 = count;
+			}
+			return count;
+		}else{
+			return 1;
+		}
+	}
 }
