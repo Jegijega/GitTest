5f971edcadac0dfe13077377cd1d1889b6f87d18
S <s>
2023-11-27 17:47:20 +0400

    {"types":"C","runs":139}

--- Problem40.java
+++ Problem40.java
@@ -1,4 +1,21 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class Problem40 {
-
+public class Problem40 extends GraphicsProgram{
+	private static final int RAD = 15;
+	public void run(){
+		GOval ball = new GOval(2 * RAD, 2 * RAD);
+		ball.setFilled(true);
+		add(ball, 0, getHeight() / 2 - RAD);
+		while(true){	
+			while(ball.getX() < getWidth() - 2 * RAD){
+				ball.move(2, 0);
+				pause(5);	
+			}
+			while(ball.getX() > 0){
+				ball.move(-2,  0);
+				pause(5);
+			}
+		}		
+	}
 }
