d478389e4a32cf01b5307f805a0788871e73ad17
S <s>
2023-11-27 18:48:33 +0400

    {"types":"C","runs":177}

--- Problem47.java
+++ Problem47.java
@@ -1,4 +1,15 @@
+import acm.program.ConsoleProgram;
 
-public class Problem47 {
-
+public class Problem47 extends ConsoleProgram{
+	public void run(){
+		String word = readLine("Enter word: ");
+		boolean ans = true;
+		for(int i = 0; i < word.length(); i++){
+			if(word.charAt(i) != word.charAt(word.length() - 1 - i)){
+				ans = false;
+				break;
+			}
+		}
+		println(ans);
+	}
 }
