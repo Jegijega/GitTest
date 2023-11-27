b8b7c0832eb4270fe874e821dfb766bc6125087a
S <s>
2023-11-27 18:31:59 +0400

    {"types":"C","runs":170}

--- Problem43.java
+++ Problem43.java
@@ -1,47 +1,22 @@
+import java.awt.Color;
 import java.awt.event.MouseEvent;
-
-import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 
 public class Problem43 extends GraphicsProgram {
-
-	private GObject object = null;
-	private double lastX = -1;
-	private double lastY = -1;
-
+	private GOval oval = null;
 	public void run() {
-		initBall();
+		oval = new GOval(30, 30);
+		oval.setFilled(true);
+		oval.setColor(Color.GREEN);
+		add (oval, getWidth() / 2, getHeight() / 2);
 		addMouseListeners();
 	}
-
-	public void mouseDragged(MouseEvent e) {
-		println("drag");
-		if (object != null) {
-			// object.setLocation(e.getX(), e.getY());
-			double mouseX = e.getX();
-			double mouseY = e.getY();
-			if (lastX != -1 || lastY != -1) {
-				object.move(mouseX - lastX, mouseY - lastY);
-			}
-
-			lastX = mouseX;
-			lastY = mouseY;
+	
+	public void mouseDragged (MouseEvent e) {
+		if (getElementAt(e.getX(), e.getY()) == oval) {
+			oval.setLocation(e.getX() - 25, e.getY() - 25);
 		}
 	}
 
-	public void mousePressed(MouseEvent e) {
-		object = getElementAt(e.getX(), e.getY());
-		if (object == null) {
-			println("nullia");
-		} else {
-			println("ar ari null");
-		}
-	}
-
-	private void initBall() {
-		GOval ball = new GOval(50, 50);
-		ball.setFilled(true);
-		add(ball);
-	}
 }
