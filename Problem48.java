a5f196ed456ee4b9220f7ef858091080615e73b0
S <s>
2023-11-27 18:59:40 +0400

    {"types":"C","runs":180}

--- Problem48.java
+++ Problem48.java
@@ -1,4 +1,18 @@
+import acm.program.ConsoleProgram;
 
-public class Problem48 {
+public class Problem48 extends ConsoleProgram{
+	public void run(){
+		String word = readLine("Enter sentence: ");
+		for(int i = 0; i < 26; i++){
+			char ch = (char)('a' + i);
+			int ans = 0;
+			for(int j = 0; j < word.length(); j++){
+				if(word.charAt(i) == ch){
+				ans++;	
+				}
+			}
+			println(ch + " " + ans);	
+		}
+	}
 
 }
