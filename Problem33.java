7a40d2c34fb09ee862f84a5936c35f37327b35d4
S <s>
2023-11-27 11:50:02 +0400

    {"types":"C","runs":93}

--- Problem33.java
+++ Problem33.java
@@ -1,4 +1,23 @@
 
-public class Problem33 {
+import java.awt.Color;
 
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+
+public class Problem33 extends GraphicsProgram {
+	
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	
+	public void run(){
+		drawCircle();
+	}
+	
+	private void drawCircle(){
+		int R = rgen.nextInt(0, 100);
+		Color color = rgen.nextColor();
+		GOval circle = new GOval(R, R);
+		circle.setFilled(true);
+		circle.setColor(color);
+	}
 }
