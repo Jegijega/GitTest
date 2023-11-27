cf01a8da9a94a09ee8f362291c87187f0ca73d32
S <s>
2023-11-27 12:05:45 +0400

    {"types":"C","runs":100}

--- Problem35.java
+++ Problem35.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Problem35 {
-
+public class Problem35 extends ConsoleProgram{
+	
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	
+	public void run(){
+		int n = readInt("Enter amount ");
+		int sum = 0;
+		for(int i = 0; i < n; i++){
+			boolean lep = false;
+			while(lep == false){
+				lep = rgen.nextBoolean();
+				sum++;
+			}
+		}
+		println(sum / n);
+	}
 }
