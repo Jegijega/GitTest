7066a0a60f4ebeff427fde335a5fbbc9a4cd830f
S <s>
2023-11-27 11:39:34 +0400

    {"types":"C","runs":89}

--- Problem32.java
+++ Problem32.java
@@ -1,4 +1,18 @@
+import acm.program.ConsoleProgram;
 
-public class Problem32 {
-
-}
+public class Problem32 extends ConsoleProgram {
+	public void run() {
+		for(int j = 1; j <=1000000; j++){	
+			int n = readInt("Enter the Number ");
+			int div = 0;
+			for(int i = 1; i <= n; i++ ){
+				if( n % i == 0){
+					div++;
+				}
+			}
+			if(div == 2){
+				println(j);
+			}
+		}	
+	}
+}	
