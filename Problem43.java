e1812cc7247b3b8c4ce23d6dd2d0bd9392f8da87
S <s>
2023-11-27 18:26:48 +0400

    {"types":"C","runs":166}

--- Problem43.java
+++ Problem43.java
@@ -1,4 +1,47 @@
+import java.awt.event.MouseEvent;
 
-public class Problem43 {
+import acm.graphics.GObject;
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
+public class Problem43 extends GraphicsProgram {
+
+	private GObject object = null;
+	private double lastX = -1;
+	private double lastY = -1;
+
+	public void run() {
+		initBall();
+		addMouseListeners();
+	}
+
+	public void mouseDragged(MouseEvent e) {
+		println("drag");
+		if (object != null) {
+			// object.setLocation(e.getX(), e.getY());
+			double mouseX = e.getX();
+			double mouseY = e.getY();
+			if (lastX != -1 || lastY != -1) {
+				object.move(mouseX - lastX, mouseY - lastY);
+			}
+
+			lastX = mouseX;
+			lastY = mouseY;
+		}
+	}
+
+	public void mousePressed(MouseEvent e) {
+		object = getElementAt(e.getX(), e.getY());
+		if (object == null) {
+			println("null");
+		} else {
+			println("!null");
+		}
+	}
+
+	private void initBall() {
+		GOval ball = new GOval(30, 30);
+		ball.setFilled(true);
+		add(ball);
+	}
 }
