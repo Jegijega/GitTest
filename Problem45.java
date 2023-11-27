b5476e4e2af6ee70586b23cc04d0cda1e6afe190
S <s>
2023-11-27 16:53:35 +0400

    {"types":"C","runs":129}

--- Problem45.java
+++ Problem45.java
@@ -6,7 +6,7 @@ public class Problem45 extends ConsoleProgram {
 		String c = readLine("Enter character");
 		int ans = 0;
 		for(int i = 0; i < w.length(); i++){
-			if(w.charAt(i) == c.charAt(1)){
+			if(w.charAt(i) == c.charAt(0)){
 			ans++;	
 			}
 		}
