ae5d90c2e8f47295187a41940ce20852216a36f5
S <s>
2023-11-27 17:12:04 +0400

    {"types":"A","runs":131}

---
+++ Problem46.java
@@ -0,0 +1,23 @@
+import acm.program.ConsoleProgram;
+
+public class Problem46 extends ConsoleProgram {
+	public void run(){
+		String word = readLine("Enter sentence: ");
+		int max = 0;
+		char maxchar = ' ';
+		for(int j = 0; j < word.length(); j++){
+			for(int i = 0; i < word.length(); i++){
+				int counter = 0;
+				if(word.charAt(i) == word.charAt(j)){
+					counter++;
+				}
+				if(counter > max){
+					max = counter;
+					maxchar = word.charAt(j);
+				}
+			}
+		println(maxchar);
+		println(max);
+		}	
+	}
+}
