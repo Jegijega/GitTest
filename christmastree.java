041a4592fd0d793bdc9a94499420d200d886f8eb
S <s>
2023-12-01 13:12:00 +0400

    {"types":"C","runs":331}

--- christmastree.java
+++ christmastree.java
@@ -6,11 +6,18 @@ import acm.program.GraphicsProgram;
 public class christmastree extends GraphicsProgram {
 	private static final int HEIGHT = 500;
 	private static final int N_OF_BRANCH = 10;
+	private static final int FIRST_BRANCH_WIDTH = 70;
+	private static final int BRANCH_WIDTH_DIF = FIRST_BRANCH_WIDTH / N_OF_BRANCH;
 	private static final int DIF_BRANCH = HEIGHT / N_OF_BRANCH ;
 	private static final int WOOD_HEIGHT = 50;
 	private static final int WOOD_WIDTH = 45;
 	public void run() {
 		drawWood();
+		drawLefthalf();
+	}
+	private void drawLefthalf() {
+		GLine line1 = new GLine (getWidth() / 2 - WOOD_WIDTH / 2, getHeight() - WOOD_HEIGHT, getWidth() / 2 - WOOD_WIDTH / 2 - FIRST_BRANCH_WIDTH , getHeight() - WOOD_HEIGHT );
+		
 	}
 	private void drawWood() {
 		GRect woody = new GRect (WOOD_WIDTH, WOOD_HEIGHT);
