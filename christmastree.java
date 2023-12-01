11dda2f7be491c6ba56b577de5a1bba929d56266
S <s>
2023-12-01 13:00:40 +0400

    {"types":"C","runs":329}

--- christmastree.java
+++ christmastree.java
@@ -1,9 +1,11 @@
+import java.awt.Color;
+
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
 public class christmastree extends GraphicsProgram {
-	private static final int HEIGHT = 100;
-	private static final int N_OF_BRANCH = 6;
+	private static final int HEIGHT = 500;
+	private static final int N_OF_BRANCH = 10;
 	private static final int DIF_BRANCH = HEIGHT / N_OF_BRANCH ;
 	private static final int WOOD_HEIGHT = 50;
 	private static final int WOOD_WIDTH = 45;
@@ -13,6 +15,7 @@ public class christmastree extends GraphicsProgram {
 	private void drawWood() {
 		GRect woody = new GRect (WOOD_WIDTH, WOOD_HEIGHT);
 		add (woody, getWidth() / 2 - WOOD_WIDTH / 2, getHeight() - WOOD_HEIGHT);
-		
-	}
+		woody.setFilled(true);
+		woody.setColor(Color.ORANGE);
+		}
 }
