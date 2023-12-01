21afd1f75ead4d1b7033c284f9758e388765d133
S <s>
2023-12-01 13:57:29 +0400

    {"types":"C","runs":365}

--- christmastree.java
+++ christmastree.java
@@ -8,7 +8,7 @@ public class christmastree extends GraphicsProgram {
 	private static final int N_OF_BRANCH = 10;
 	private static final int FIRST_BRANCH_WIDTH = 80;
 	private static final int BRANCH_WIDTH_DIF = FIRST_BRANCH_WIDTH / N_OF_BRANCH;
-	private static final int DIF_BRANCH = 15 ;
+	private static final int DIF_BRANCH = 25 ;
 	private static final int WOOD_HEIGHT = 50;
 	private static final int WOOD_WIDTH = 45;
 	private static final int FIRST_BRANCH_HEIGHT = HEIGHT / N_OF_BRANCH + DIF_BRANCH * 5;
