01c497ce0e89bc45fda5a3608d9fbc7f8e6fcb0c
S <s>
2023-12-01 13:56:29 +0400

    {"types":"C","runs":360}

--- christmastree.java
+++ christmastree.java
@@ -22,7 +22,7 @@ public class christmastree extends GraphicsProgram {
 					getWidth() / 2 - WOOD_WIDTH / 2 - FIRST_BRANCH_WIDTH + BRANCH_WIDTH_DIF * i, getHeight() - WOOD_HEIGHT - DIF_BRANCH * i);
 			add(line1);
 			GLine line2 = new GLine (getWidth() / 2 - WOOD_WIDTH / 2 - FIRST_BRANCH_WIDTH + BRANCH_WIDTH_DIF * i, getHeight() - WOOD_HEIGHT - DIF_BRANCH * i,
-					getWidth() / 2 - WOOD_WIDTH /2, getHeight() - WOOD_HEIGHT - DIF_BRANCH * i - DIF_BRANCH);
+					getWidth() / 2 - WOOD_WIDTH /2, getHeight() - WOOD_HEIGHT - DIF_BRANCH * i - DIF_BRANCH - FIRST_BRANCH_HEIGHT);
 			add(line2);
 		}
 	}
