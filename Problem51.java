5397b45a125eb456c23fd94a7f53727ee219fbd9
S <s>
2023-12-04 12:32:44 +0400

    {"types":"C","runs":483}

--- Problem51.java
+++ Problem51.java
@@ -1,4 +1,14 @@
+import acm.program.ConsoleProgram;
 
-public class Problem51 {
-
+public class Problem51 extends ConsoleProgram{
+	public void run(){
+		String a = readLine("Enter String: ");
+		for(int i = 0; i < a.length(); i++){
+			for(int j = 0; j < a.length(); j++){
+				if(a.charAt(i) == a.charAt(j)){
+					a = a.substring(0, j) + a.substring(j + 1);
+				}
+			}
+		}
+	}
 }
