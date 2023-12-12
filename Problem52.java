c7c0540a1ef45b0252db1e8a2bd42e7c8616c2af
S <s>
2023-12-12 19:26:54 +0400

    {"types":"C","runs":1}

--- Problem52.java
+++ Problem52.java
@@ -1,4 +1,26 @@
+import java.io.FileReader;
+import java.io.IOException;
 
-public class Problem52 {
+import acm.program.ConsoleProgram;
 
+public class Problem52 extends ConsoleProgram{
+	public void run(){
+		FileReader rd = null;
+		try{
+			rd = new FileReader("Problem52.java");
+			while(true){
+				int ch = rd.read();
+				if(ch != -1)break;
+				print((char)ch);
+			}
+		}catch(IOException e){
+			e.getStackTrace();
+		}finally{
+			try {
+				rd.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
