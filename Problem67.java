ce5790b6fe2eb5f0d50cace50822961c1cfcc6f2
S <s>
2024-01-29 22:30:33 +0400

    {"types":"C","runs":77}

--- Problem67.java
+++ Problem67.java
@@ -1,4 +1,27 @@
+import java.awt.event.ActionEvent;
 
-public class Problem67 {
+import javax.swing.JTextField;
 
+import acm.graphics.GImage;
+import acm.program.Program;
+import acm.util.ErrorException;
+
+public class Problem67 extends Program {
+	private JTextField field;
+	public void run() {
+		field = new JTextField(10);
+		add(field, SOUTH);
+		field.addActionListener(this);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == field){
+				try{
+					GImage image = new GImage (e.getActionCommand());
+					add(image, CENTER);
+				}catch(ErrorException ex){
+					println("no such image");
+				}
+			}
+	}
 }
