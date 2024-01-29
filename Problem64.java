1336df9874d0792efb90ffca3b768018f4632396
S <s>
2024-01-29 21:25:34 +0400

    {"types":"C","runs":61}

--- Problem64.java
+++ Problem64.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class Problem64 {
+import acm.program.ConsoleProgram;
 
+public class Problem64 extends ConsoleProgram{
+	private HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+	public void run(){
+		String name1 = readLine("Enter first name: ");
+		String name2 = readLine("Enter second name: ");
+		while(!name1.equals("") || !name2.equals("")){
+			if(!map.containsKey(name1)){
+				ArrayList<String> arr = new ArrayList<String>();
+				arr.add(name2);
+				map.put(name1, arr);
+			}else{
+				map.get(name1).add(name2);
+			}
+			name1 = readLine("Enter first name: ");
+			if(name1.equals("")){
+				break;
+			}
+			name2 = readLine("Enter second name: ");
+		}
+		for(String key : map.keySet()){
+			println(key + ": " + map.get(key).toString());
+		}
+	}
 }
