4cb0478c53ae18f8191a4250a2d6f9c9f9954487
S <s>
2024-01-29 21:15:02 +0400

    {"types":"C","runs":59}

--- Problem63.java
+++ Problem63.java
@@ -1,4 +1,28 @@
+import java.util.HashMap;
 
-public class Problem63 {
+import acm.program.ConsoleProgram;
 
+public class Problem63 extends ConsoleProgram{
+	private HashMap<String, Integer> map = new HashMap<String, Integer>();
+	public void run(){
+		String name1 = readLine("Enter first name: ");
+		String name2 = readLine("Enter second name: ");
+		while(!name1.equals("") || !name2.equals("")){
+			if(!map.containsKey(name1)){
+				map.put(name1, 1);
+			}else{
+				map.replace(name1, map.get(name1) + 1);
+			}
+			name1 = readLine("Enter first name: ");
+			name2 = readLine("Enter second name: ");
+		}
+		int most = 0;
+		String outgoing = "";
+		for(String key : map.keySet()){
+			if(map.get(key) > most){
+				most = map.get(key);
+				outgoing = key;
+			}
+		}
+	}
 }
