3822cdc66f5794941e9e54ffeafe8442af0c3496
S <s>
2024-01-29 20:41:32 +0400

    {"types":"C","runs":57}

--- Problem62.java
+++ Problem62.java
@@ -6,13 +6,13 @@ public class Problem62 extends ConsoleProgram{
 	private HashMap<String,Integer> map = new HashMap<String, Integer>();
 	public void run(){
 		String name = readLine("Enter name: ");
-		while(!name.equals("")){
-			print(1);
+		while(!name.equals("")){=
 			if(!map.containsKey(name)){
 				map.put(name, 1);
 			}else{
 				map.replace(name, map.get(name) + 1);
 			}
+			name = readLine("Enter name: ");
 		}
 		for(String key : map.keySet()){
 			println(key + ": " + map.get(key));
