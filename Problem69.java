806ce79e93282dc54aa13d85b98f3476d2c1005f
S <s>
2024-01-30 22:59:27 +0400

    {"types":"C","runs":128}

--- Problem69.java
+++ Problem69.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
 
-public class Problem69 {
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
 
+public class Problem69 extends GraphicsProgram{
+	public void run(){
+		ArrayList<GLabel> list = new ArrayList<>();
+		for(int i = 0; i < 100; i++){
+			GLabel label = new GLabel ("I am new");
+			add(label, getWidth() / 2 - label.getWidth() / 2, getHeight());
+			list.add(label);
+			for(int j = 0; j < list.size(); j++){
+				list.get(j).move(0, -10);
+			}
+		}
+	}
 }
