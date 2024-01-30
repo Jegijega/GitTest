0473bb3dd19b80f4d330af5b6d006308515d6719
S <s>
2024-01-31 00:05:37 +0400

    {"types":"C","runs":134}

--- Problem70.java
+++ Problem70.java
@@ -1,4 +1,20 @@
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
-public class Problem70 {
+public class Problem70 extends GraphicsProgram{
+	public void run(){
+		update();
+	}
 
+	private void update() {
+		removeAll();
+		for(int i = 0; i < 10; i++){
+			GLine line = new GLine(i * getWidth() / 10, 0, i * getWidth() / 10, getHeight());
+			add(line);
+		}
+		for(int i = 0; i < 10; i++){
+			GLine line = new GLine( 0, i * getHeight() / 10, getWidth(), i * getHeight() / 10);
+			add(line);
+		}
+	}
 }
